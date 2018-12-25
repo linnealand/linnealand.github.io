afb55371fc908bfb8e0a8b2afdd91265c3774414
Linnea <linneallindstrom@hotmail.com>
2018-12-24 21:26:51 -0500

    revamp - gradient theme

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Smooth scrolling
 $("nav").find("a").click(function(e) {
     e.preventDefault();
     var section = $(this).attr("href");
@@ -5,3 +6,34 @@ $("nav").find("a").click(function(e) {
         scrollTop: $(section).offset().top
     });
 });
+
+
+// Project img slideshow
+  var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
